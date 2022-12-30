ae07e5cac0490c5774e411fee92b4e9806a64fd7
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2022-12-30 02:44:14 -0500

    fixing styling issues

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const plotSomeData = (data, title='Graph Title') => {
+const hide = (element) => {
+    element.setAttribute('hidden', '');
+};
+
+const show = (element) => {
+    element.removeAttribute('hidden');
+};
+
+const plotSomeData = (data, title='') => {
     var layout = {
         width: window.innerWidth,
         height: window.innerHeight,
@@ -11,32 +19,49 @@ const plotSomeData = (data, title='Graph Title') => {
 const processData = (rows) => {
     const x = [], text = [];
 
-    // sort the data by time
-    const sortedRows = rows.sort((a, b) => a.day_time - b.day_time)
+    // sort the data by provided x value
+    // [...document.querySelectorAll('input[name="xAxisChoice"]')].find((el) => el.checked)?.dataset?.header
 
-    const graphHeaders = document.getElementById('graphHeaders');
-    const labelList = [...graphHeaders.querySelectorAll('label')];
+    const xVariableElement = [...document.querySelectorAll('input[name="xAxisChoice"]')]?.find((el) => el.checked);
+    const xVariable = xVariableElement?.dataset?.header;
+    if (xVariable === undefined) {
+        alert('You have to select a xAxis for this to work');
+        throw new Error('Super smart user is trying to graph without selecting an x value')
+    }
+    const sortedRows = rows.sort((a, b) => a[xVariable] - b[xVariable])
+
+    const checkboxes = [...document.querySelectorAll('input[type="checkbox"]')];
     const allYs = {};
-    labelList.forEach((label) => {
-        const checkbox = label.querySelector('input[type="checkbox"]');
+    let userChoseAYValue = false;
+    checkboxes.forEach((checkbox) => {
         const {checked, name} = checkbox;
         if (checked) {
             allYs[name] = [];
+            userChoseAYValue = true;
         }
     });
+
+    if (!userChoseAYValue) {
+        alert('You have to select values for the yAxis for this to work');
+        throw new Error('Super smart user is trying to graph without selecting any y values')
+    }
 
     // clear the interface now that we've graphed
     document.getElementById('interface').innerHTML = "";
 
-    sortedRows.forEach((row, dayCounter) => {
-        x.push(dayCounter);
+    sortedRows.forEach((row) => {
+        x.push(row[xVariable]);
 
         Object.keys(allYs).forEach((header) => {
             allYs[header].push(row[header]);
         });
 
-        const date = new Date(parseFloat(row.day_time));
-        text.push(date.toLocaleString())
+        if (Object.prototype.hasOwnProperty.call(row, 'day_time')) {
+            const date = new Date(parseFloat(row.day_time));
+            text.push(date.toLocaleString())
+        } else {
+            text.push('');
+        }
     });
 
     const traces = Object.keys(allYs).map((yLabel) => ({
@@ -49,11 +74,14 @@ const processData = (rows) => {
     }))
 
     return traces;
-}
+};
 
 const makePlot = (input) => {
     // remove any padding, scrolling, or margin on the body tag
     document.querySelector('body').style = "margin: 0; padding: 0; overflow: hidden;";
+    // hide the interface and show the graph
+    hide(document.getElementById('interface'))
+    show(document.getElementById('graphHolder'))
 
     d3.csv(input, (rows) => {
         const traces = processData(rows);
@@ -62,23 +90,47 @@ const makePlot = (input) => {
 };
 
 window.onload = () => {
+    show(document.getElementById('interface'));
     const docPicker = document.getElementById('docPicker');
     const graphButton = document.getElementById('startGraphing');
-    graphButton.addEventListener('click', () => {
-        // use the processed data to do thing
-        const fileList = docPicker.files;
-        const file = fileList[fileList.length - 1];
 
-        const fileReader = new FileReader();
-        fileReader.addEventListener('load', (event) => {
-            makePlot(event.target.result);
-        });
-
-        fileReader.readAsDataURL(file);
-
+    let clicked = false;
+    graphButton.addEventListener('mousedown', (event) => {
+        event.target.classList.add('clicked');
+        event.target.classList.remove('notClicked');
+    
+        clicked = true;
     });
+    
+    const resetClick = (event) => {
+        if (clicked) {
+            event.target.classList.add('notClicked');
+            event.target.classList.remove('clicked');              
+            
+            // use the processed data to do thing
+            const fileList = docPicker.files;
+            const file = fileList[fileList.length - 1];
+    
+            const fileReader = new FileReader();
+            fileReader.addEventListener('load', (event) => {
+                makePlot(event.target.result);
+            });
+    
+            fileReader.readAsDataURL(file);
+
+            clicked = false;
+        }
+    }
+
+    graphButton.addEventListener('mouseup', resetClick);
+    graphButton.addEventListener('mouseout', resetClick);
+
 
     docPicker.addEventListener('change', (event) => {
+        show(document.getElementById('selectionInterface'));
+        show(document.getElementById('startGraphing'))
+        document.getElementById('startGraphing').style.display = 'block';
+
         const fileList = event.target.files;
         const file = fileList[fileList.length - 1];
         
@@ -90,14 +142,21 @@ window.onload = () => {
                     const headers = Object.keys(data[0]);
                     // display as a checkbox list of which headers you want to choose
                     const headersHTML = headers.map((header) => {
-                        return `<label for="${header}">
-                        <input type="checkbox" id="${header}" name="${header}"> ${header}
+                        return `<label for="${header}-header">
+                        <input type="checkbox" id="${header}-header" name="${header}"> ${header}
+                        </label>`
+                    })
+                    const xAxisVariableHTML = headers.map((header) => {
+                        return `<label for="${header}-axis">
+                        <input type="radio" id="${header}-axis" data-header="${header}" name="xAxisChoice"> ${header}
                         </label>`
                     })
 
                     
                     const graphHeaders = document.getElementById('graphHeaders');
-                    graphHeaders.innerHTML = '<h3>Choose headers</h3>' + headersHTML.join('<br/>');
+                    const xAxisVariable = document.getElementById('xAxisVariable');
+                    graphHeaders.innerHTML = headersHTML.join('<br/>');
+                    xAxisVariable.innerHTML = xAxisVariableHTML.join('<br/>')
                 }
             });
         })
